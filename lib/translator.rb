f0b7387c59d1cb6573cd219b0bf9e8d9bc067a1e
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-25 20:03:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_japanese_emoticon(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
     emoji_Dictionary = load_library(file_path)
 
   emoji_Dictionary.each do |topLevel_Key, topLevel_Value|
