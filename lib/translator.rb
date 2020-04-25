7c815dbe8d64a544dd2e3334b372bae01681cdd5
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-25 18:39:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,19 @@ emojis = YAML.load_file('./lib/emoticons.yml')
 
 def load_library(file_path)
   emojis = YAML.load_file(file_path)
+  organized_emojis = {}
+
+  emojis.each do |key, value|
+    
+    if !organized_emojis.include?(key)
+      organized_emojis[key] = {}
+    end
+
+    if !organized_emojis[key].include?(value[0])
+        organized_emojis[key] = {english: value[0], japanese: value[1]}
+    end
+  end 
+  return organized_emojis
 end
 
 def get_japanese_emoticon
